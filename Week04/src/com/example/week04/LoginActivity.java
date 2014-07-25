7e4edd968b24d76f9e36006ca07ae7e20e75386b
Jihwan Jeon <jihwan0321@kaist.ac.kr>
2014-07-25 20:55:18 +0900

    Bug in GCM service fixed.

--- Week04/src/com/example/week04/LoginActivity.java
+++ Week04/src/com/example/week04/LoginActivity.java
@@ -40,6 +40,7 @@ public class LoginActivity extends Activity {
 	// Project number of API Console for GCM pushing.
 	String SENDER_ID = "402485531539";
 	GoogleCloudMessaging gcm;
+	String regId = "";
 	Context mContext;
 	
 	@Override
@@ -48,14 +49,22 @@ public class LoginActivity extends Activity {
 		mContext = getApplicationContext();
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
+		// get GCM token in background.
+		if (checkPlayServices()) {
+			gcm = GoogleCloudMessaging.getInstance(LoginActivity.this);
+			regId = getRegistrationId(mContext);
+		}
+        if (regId.isEmpty()) {
+            registerInBackground();
+        }
+		
 		// Check preference for autoLogin check. if there is. try to login.
 		SharedPreferences appPref = getSharedPreferences("appPref", 0);
 		String autoLoginId = appPref.getString("loginID", "");
-		String autoLoginGCMId = appPref.getString("gcmToken", "");
-		if( !autoLoginId.isEmpty() && !autoLoginGCMId.isEmpty()) {
+		if( !autoLoginId.isEmpty() && !regId.isEmpty() ) {
 			String autoLoginPassword = appPref.getString("loginPassword", ""); 
 			try {
-				String loginResult = new doLogin().execute(autoLoginId, autoLoginPassword, autoLoginGCMId).get();
+				String loginResult = new doLogin().execute(autoLoginId, autoLoginPassword, regId).get();
 				if( loginResult.equals("success") ) {
 					startApp();
 				}
@@ -78,20 +87,11 @@ public class LoginActivity extends Activity {
 				String passInput = password.getText().toString();
 				
 				try {
-					// get GCM token.
-					if (checkPlayServices()) {
-						gcm = GoogleCloudMessaging.getInstance(LoginActivity.this);
-			        } else {
-			            Log.i("Google Play APK Checking", "No valid Google Play Services APK found.");
-			            finish();
-			        }
-					String gcmToken = getRegistrationId(mContext);
-							
 					// connect to server and verify id and password.
 					if( emailInput.isEmpty() ) {
 						Toast.makeText(LoginActivity.this, "Login failed :: Blanked ID field!", Toast.LENGTH_SHORT).show();
 					}
-					else if( gcmToken.isEmpty() ) {
+					else if( regId.isEmpty() ) {
 						Toast.makeText(LoginActivity.this, "Login failed :: Blanked Token field!", Toast.LENGTH_SHORT).show();
 					}
 					else {
@@ -124,50 +124,7 @@ public class LoginActivity extends Activity {
 		});
 	}
 	
-	private class doLogin extends AsyncTask<String, Void, String> {
-		@Override
-    	protected String doInBackground(String... arg) {
-    		String email = new String(arg[0]);
-    		String password = new String(arg[1]);
-    		String gcmid = new String(arg[2]);
-    		
-    		String serverURL = new Settings().getServerURL();
-    		String URL = serverURL + "login";
-    		
-    		DefaultHttpClient client = new DefaultHttpClient();
-    		try {
-    			// Make NameValuePair for password to server. use POST method.
-				ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-				nameValuePairs.add(new BasicNameValuePair("useremail", email));
-				nameValuePairs.add(new BasicNameValuePair("password", password));
-				nameValuePairs.add(new BasicNameValuePair("regid", gcmid));
-    			
-    			// Make connection to server.
-    			HttpParams connectionParams = client.getParams();
-    			HttpConnectionParams.setConnectionTimeout(connectionParams, 5000);
-    			HttpConnectionParams.setSoTimeout(connectionParams, 5000);
-    			HttpPost httpPost = new HttpPost(URL);
-    			
-    			// Send NameValuePair to server.
-				UrlEncodedFormEntity entityRequest = new UrlEncodedFormEntity(nameValuePairs, "EUC-KR");
-				httpPost.setEntity(entityRequest);
-    			
-    			// Get response and parse entity.
-    			HttpResponse responsePost = client.execute(httpPost);
-    			HttpEntity resEntity = responsePost.getEntity();
-    			
-    			// Parse result to string.
-    			String result = EntityUtils.toString(resEntity);
-    			client.getConnectionManager().shutdown();
-    			return result;
-    			
-    		} catch (Exception e) {
-    			e.printStackTrace();
-    			client.getConnectionManager().shutdown();	// Disconnect.
-    			return "";
-    		}
-    	}
-    }
+	
 	
 	void setAutoLogin(final String id, final String password) {
 		new AsyncTask<Void, Void, Void>() {
@@ -211,7 +168,6 @@ public class LoginActivity extends Activity {
         String registrationId = prefs.getString("gcmToken", "");
         if (registrationId.isEmpty()) {
             Log.i("Getting GCM reg-ID", "Registration not found. try to get from GCM server.");
-            return registerInBackground();
         }
         // Check if app was updated; if so, it must clear the registration ID
         // since the existing regID is not guaranteed to work with the new
@@ -220,7 +176,6 @@ public class LoginActivity extends Activity {
         int currentVersion = getAppVersion(context);
         if (registeredVersion != currentVersion) {
             Log.i("Getting GCM reg-ID", "App version changed. try to get from GCM server.");
-            return registerInBackground();
         }
         return registrationId;
     }
@@ -244,51 +199,32 @@ public class LoginActivity extends Activity {
      * Stores the registration ID and the app versionCode in the application's
      * shared preferences.
      */
-    private String registerInBackground() {
-    	try {
-    		return new AsyncTask<Void, Void, String>() {
-		        @Override
-		        protected String doInBackground(Void... params) {
-		        	String regid = "";
-		            try {
-		                if (gcm == null) {
-		                    gcm = GoogleCloudMessaging.getInstance(mContext);
-		                }
-		                regid = gcm.register(SENDER_ID);
+    private void registerInBackground() {
+		new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(Void... params) {
+				try {
+					if (gcm == null) {
+						gcm = GoogleCloudMessaging.getInstance(mContext);
+					}
+					regId = gcm.register(SENDER_ID);
 
-
-		                // For this demo: we don't need to send it because the device will send
-		                // upstream messages to a server that echo back the message using the
-		                // 'from' address in the message.
-
-		                // Persist the regID - no need to register again.
-		                SharedPreferences appPref = getSharedPreferences("appPref", 0);
-		        		SharedPreferences.Editor edit = appPref.edit();
-		        		edit.putString("gcmToken", regid);
-		        		edit.putInt("appVersion", getAppVersion(mContext));
-		        		edit.commit();
-		            } catch (IOException ex) {
-		            	ex.printStackTrace();
-		            	Log.i("Connecting GCM server", "Error :" + ex.getMessage());
-		                regid = "";
-		                // If there is an error, don't just keep trying to register.
-		                // Require the user to click a button again, or perform
-		                // exponential back-off.
-		            }
-		            return regid;
-		        }
-
-		        @Override
-		        protected void onPostExecute(String msg) {
-		            Log.i("registering", msg);
-		        }
-		    }.execute(null, null, null).get();
-		}
-    	catch (Exception e) {
-    		e.printStackTrace();
-    		Log.i("Connecting GCM server", "Error :" + e.getMessage());
-			return "";
-		}
+					// Persist the regID - no need to register again.
+					SharedPreferences appPref = getSharedPreferences("appPref", 0);
+					SharedPreferences.Editor edit = appPref.edit();
+					edit.putString("gcmToken", regId);
+					edit.putInt("appVersion", getAppVersion(mContext));
+					edit.commit();
+				} catch (IOException ex) {
+					ex.printStackTrace();
+					Log.i("Connecting GCM server", "Error :" + ex.getMessage());
+					// If there is an error, don't just keep trying to register.
+					// Require the user to click a button again, or perform
+					// exponential back-off.
+				}
+				return null;
+			}
+		}.execute(null, null, null);
     }
 	
 	void startApp() {
@@ -302,5 +238,49 @@ public class LoginActivity extends Activity {
 		}
 		startActivity(intent);
         finish();
+	}
+	
+	public class doLogin extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... arg) {
+			String email = new String(arg[0]);
+			String password = new String(arg[1]);
+				
+			String serverURL = new Settings().getServerURL();
+			String URL = serverURL + "login";
+				
+			DefaultHttpClient client = new DefaultHttpClient();
+			try {
+				// Make NameValuePair for password to server. use POST method.
+				ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+				nameValuePairs.add(new BasicNameValuePair("useremail", email));
+				nameValuePairs.add(new BasicNameValuePair("password", password));
+				nameValuePairs.add(new BasicNameValuePair("regid", regId));
+				
+				// Make connection to server.
+				HttpParams connectionParams = client.getParams();
+				HttpConnectionParams.setConnectionTimeout(connectionParams, 5000);
+				HttpConnectionParams.setSoTimeout(connectionParams, 5000);
+				HttpPost httpPost = new HttpPost(URL);
+				
+				// Send NameValuePair to server.
+				UrlEncodedFormEntity entityRequest = new UrlEncodedFormEntity(nameValuePairs, "EUC-KR");
+				httpPost.setEntity(entityRequest);
+				
+				// Get response and parse entity.
+				HttpResponse responsePost = client.execute(httpPost);
+				HttpEntity resEntity = responsePost.getEntity();
+					
+				// Parse result to string.
+				String result = EntityUtils.toString(resEntity);
+				client.getConnectionManager().shutdown();
+				return result;
+					
+			} catch (Exception e) {
+				e.printStackTrace();
+				client.getConnectionManager().shutdown();	// Disconnect.
+				return "";
+			}
+		}
 	}
 }
